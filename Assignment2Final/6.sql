16ccec5a2f3dfaf0f7100c41c7598a943834c8f7
Nicholas Serrano <34073804+nickroxcks@users.noreply.github.com>
2018-11-08 12:09:52 -0700

    Update 6.sql

--- Assignment2Final/6.sql
+++ Assignment2Final/6.sql
@@ -39,22 +39,6 @@ ON (b.city = c.city);
 
 
 
--- OLD WORK COULDNT UNDERSTAND
---SELECT L1.city,L1.prov, r1.src, COUNT(r1.src) AS RidesFrom
---FROM rides r1, locations L1
---WHERE L1.lcode = r1.src
---GROUP BY L1.city
-
---SELECT L2.city,L2.prov, r2.dst, COUNT(r2.dst) AS RidesTo
---FROM rides r2, locations L2
---WHERE L2.lcode = r2.dst
---GROUP BY L2.city;
-
---SELECT L3.city, L3.prov, enroute.lcode, COUNT(enroute.lcode) AS NumEnroute
---FROM enroute, locations L3
---WHERE L3.lcode = enroute.lcode
---GROUP BY L3.city;
-
 
 
 
